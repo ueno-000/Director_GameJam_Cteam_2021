2a6bd3dfcbe64c338abc25395a1c336d06033133
nogu06 <knocktokotoko@gmail.com>
2021-08-07 19:36:36 +0900

    爆弾生成処理を書いた

--- Assets/Spricts/Noguchi/BombGenarater.cs
+++ Assets/Spricts/Noguchi/BombGenarater.cs
@@ -10,27 +10,54 @@ public class BombGenarater : MonoBehaviour
     [SerializeField] Vector2 upperLeft = new Vector2(-0.5f, -0.5f);
     [SerializeField] Vector2 lowerRight = new Vector2(0.5f, 0.5f);
 
+    [SerializeField] float timeCount = 0;
+    float generateTime = 5.0f;
+
     private void Start()
     {
         Setup();
     }
 
-    public void Setup()
+    public void Update()
     {
-        for (int i = 0; i < numberOfInitialGeneration; i++)
-        {
-            GameObject generateBomb = Instantiate(bombPrefab.gameObject);
+        timeCount += Time.deltaTime;
 
+        if (timeCount >= generateTime)
+        {
+            timeCount = 0;
+            if (generateTime > 0.2f)
+            {
+                generateTime -= 0.3f;
+            }
+            else if (generateTime != 0.2f)
+            {
+                generateTime = 0.2f;
+            }
             float generatePosX = Random.Range(upperLeft.x, lowerRight.x);
             float generatePosY = Random.Range(upperLeft.y, lowerRight.y);
-
-            var worldPoint = transform.TransformPoint(new Vector2(generatePosX,generatePosY));
-
-            generateBomb.transform.position = worldPoint;
+            GenerateBomb(generatePosX, generatePosY);
         }
-
-
     }
 
 
+    public void Setup()
+    {
+
+        for (int i = 0; i < numberOfInitialGeneration; i++)
+        {
+            float generatePosX = Random.Range(upperLeft.x, lowerRight.x);
+            float generatePosY = Random.Range(upperLeft.y, lowerRight.y);
+
+            GenerateBomb(generatePosX, generatePosY);
+        }
+    }
+
+    public void GenerateBomb(float x, float y)
+    {
+        GameObject generateBomb = Instantiate(bombPrefab.gameObject);
+
+        var worldPoint = transform.TransformPoint(new Vector2(x, y));
+
+        generateBomb.transform.position = worldPoint;
+    }
 }
