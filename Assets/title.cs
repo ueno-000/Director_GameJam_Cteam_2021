fa6007991cec0235585f667a351e4cc8aa89af62
arn_gp21 <reogaeru0303@gmail.com>
2021-08-07 16:09:21 +0900

    タイトル画面（仮）

---
+++ Assets/title.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class title : MonoBehaviour
+{
+    Image Image;
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("result");
+        }
+    }
+}
