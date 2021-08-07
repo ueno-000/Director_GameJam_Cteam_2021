cbd37308b3caf7ef44a11cd51d0b9f75259de32f
nogu06 <knocktokotoko@gmail.com>
2021-08-07 17:45:44 +0900

    爆弾の処理を書いた

---
+++ Assets/Spricts/Noguchi/Bomb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] int hp = 50;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            hp--;
+            if (hp <= 0)
+            {
+                Explosion();
+                Destroy(this.gameObject);
+                Destroy(this);
+            }
+        }
+    }
+
+    public void Explosion()
+    {
+        //ジェネレート
+
+    }
+
+}
---
+++ Assets/Spricts/Noguchi/BombGenarater.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombGenarater : MonoBehaviour
+{
+    [SerializeField] Bomb bombPrefab;
+    [SerializeField] int numberOfInitialGeneration = 5;
+
+    [SerializeField] Vector2 upperLeft = new Vector2(-0.5f, -0.5f);
+    [SerializeField] Vector2 lowerRight = new Vector2(0.5f, 0.5f);
+
+    private void Start()
+    {
+        Setup();
+    }
+
+    public void Setup()
+    {
+        for (int i = 0; i < numberOfInitialGeneration; i++)
+        {
+            GameObject generateBomb = Instantiate(bombPrefab.gameObject);
+
+            float generatePosX = Random.Range(upperLeft.x, lowerRight.x);
+            float generatePosY = Random.Range(upperLeft.y, lowerRight.y);
+
+            var worldPoint = transform.TransformPoint(new Vector2(generatePosX,generatePosY));
+
+            generateBomb.transform.position = worldPoint;
+        }
+
+
+    }
+
+
+}
