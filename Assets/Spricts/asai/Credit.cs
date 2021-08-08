363f9fec478a5ca6dd797159484e8588fdadcc64
arn_gp21 <reogaeru0303@gmail.com>
2021-08-08 13:06:25 +0900

    タイトルシーンとそれぞれのリザルトシーン

---
+++ Assets/Spricts/asai/Credit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Credit : MonoBehaviour
+{
+    public GameObject image;
+    bool creditdispryed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (creditdispryed == true)
+            {
+                image.SetActive(false);
+                creditdispryed = false;
+            }
+            else
+            {
+                image.SetActive(true);
+                creditdispryed = true;
+            }            
+        }      
+    }
+}
---
+++ Assets/Spricts/asai/Text_blinking.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text_blinking : MonoBehaviour
+{
+    private Text text;
+
+    [SerializeField]
+    float blinking = 0.1f;
+
+    float text_transparent;
+    void Start()
+    {
+        text = this.gameObject.GetComponent<Text>();
+    }
+    void Update()
+    {
+        text.color = GetAlphaColor(text.color);
+    }
+   
+    Color GetAlphaColor(Color color)
+    {
+        text_transparent += Time.deltaTime * 5.0f * blinking;
+        color.a = Mathf.Sin(text_transparent) * 0.5f + 0.5f;
+
+        return color;
+    }
+}
