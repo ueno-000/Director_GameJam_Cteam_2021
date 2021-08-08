d55c045842e1f4d0e3d306d27d6377932cdd0dec
uen0_ <sarasa4869@zoho.com>
2021-08-08 15:00:14 +0900

    更新

--- Assets/Spricts/Ueno/PlayerController.cs
+++ Assets/Spricts/Ueno/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// シューティングゲームの自機を操作するためのコンポーネント
@@ -20,36 +21,72 @@ public class PlayerController : MonoBehaviour
     //Helthの設定
     [SerializeField] int hp = 5;
     [SerializeField]HelthController helth;
-    //Animator m_anim;
-    //AudioSource m_audio = default;
+    [SerializeField] PlayerController enemy;
+    [SerializeField] KeyCode key;
+    [SerializeField] int id;
+  
+    AudioSource m_audio = default;
+    AudioClip se;
     void Start()
     {
         m_rb = GetComponent<Rigidbody2D>();
-        //m_anim = GetComponent<Animator>();
-        //m_audio = GetComponent<AudioSource>();
+        m_audio = GetComponent<AudioSource>();
+        se = GetComponent<AudioClip>();
     }
 
     void Update()
     {
+
+        float h = 0;
+        float v = 0;
+
         // 自機を移動させる
-        float h = Input.GetAxisRaw("Horizontal");   // 垂直方向の入力を取得する
-        float v = Input.GetAxisRaw("Vertical");     // 水平方向の入力を取得する
+        if (id == 1)
+        {
+             h = Input.GetAxisRaw("Horizontal");   // 垂直方向の入力を取得する
+             v = Input.GetAxisRaw("Vertical");     // 水平方向の入力を取得する
+           
+        }
+        else
+        {
+            h = Input.GetAxisRaw("Horizontal2");   // 垂直方向の入力を取得する
+            v = Input.GetAxisRaw("Vertical2");     // 水平方向の入力を取得する
+        }
         Vector2 dir = new Vector2(h, v).normalized; // 進行方向の単位ベクトルを作る (dir = direction) 
         m_rb.velocity = dir * m_moveSpeed;        // 単位ベクトルにスピードをかけて速度ベクトルにして、それを Rigidbody の速度ベクトルとしてセットする
 
-        // 左クリックまたは左 Ctrl で弾を発射する（単発）
-        if (Input.GetButtonDown("Fire1"))
+        // 弾を発射する（単発）
+
+        if (id == 1)
         {
-            Fire1();
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Fire1();
+                m_audio.PlayOneShot(m_audio.clip);
+            }
+        }
+        if (id == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                Fire1();
+                m_audio.Play();
+            }
         }
 
-        //if (Input.GetButton("Fire2"))
-        //{
-        //    if (m_bulletLimit == 0 || this.GetComponentsInChildren<PlayerBulletController>().Length < m_bulletLimit)    // 画面内の弾数を制限する
-        //    {
-        //        Fire1();
-        //    }
-        //}
+        //HP0でシーン遷移
+
+        if (id==1&&hp==0)
+        {
+            Debug.Log("Player1:HPが０になってしまった");
+            SceneManager.LoadScene("result_p2_win");
+        }
+        if (id == 2 && hp == 0)
+        {
+            Debug.Log("Player2:HPが０になってしまった");
+            SceneManager.LoadScene("result_p1_win");
+        }
+
     }
 
     /// <summary>
@@ -61,18 +98,41 @@ public class PlayerController : MonoBehaviour
         {
             GameObject go = Instantiate(m_bulletPrefab, m_muzzle.position, m_bulletPrefab.transform.rotation);  // インスペクターから設定した m_bulletPrefab をインスタンス化する
             go.transform.SetParent(this.transform);
-
+            go.GetComponent<BulletController>().enemy = this.enemy;
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Bullet") 
+        if (id == 1)
+        {
+            if (collision.gameObject.tag == "Bullet2")
             {
-            hp = hp - 1;
-            Debug.Log("hpが減った");
-            helth.UpdateSlider(hp);
+                hp = hp - 1;
+                m_audio.Play();
+                Debug.Log("Player1:hpが減った");
+                helth.UpdateSlider(hp);
             }
-     }
-    
+        }
+        if(id ==2)
+        {
+            if (collision.gameObject.tag == "Bullet")
+            {
+                hp = hp - 1;
+                m_audio.Play();
+                Debug.Log("Player2:hpが減った");
+                helth.UpdateSlider(hp);
+            }
+        }
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bubble")
+        {
+            hp = hp - 1;
+            //m_audio.Play();
+            Debug.Log("爆弾ダメージ");
+            helth.UpdateSlider(hp);
+        }
+    }
 }
