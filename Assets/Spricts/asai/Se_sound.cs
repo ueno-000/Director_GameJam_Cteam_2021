7e6d0c9c5578cd1cde2834c7749b6986c1cce819
arn_gp21 <reogaeru0303@gmail.com>
2021-08-08 15:31:04 +0900

    タイトルからゲーム画面からリザルト一通り完成

---
+++ Assets/Spricts/asai/Se_sound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Se_sound : MonoBehaviour
+{
+    private AudioSource sound0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound0 = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            sound0.PlayOneShot(sound0.clip);
+
+        }
+    }
+}
