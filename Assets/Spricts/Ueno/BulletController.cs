96943ffe8ffa7d92c2d725cfe726d6808634eeb1
uen0_ <sarasa4869@zoho.com>
2021-08-08 15:07:07 +0900

    更新

---
+++ Assets/Spricts/Ueno/BulletController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵の弾を制御するコンポーネント
+/// 出現時のプレイヤーの位置を検出して、その方向に等速直線運動する
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class BulletController : MonoBehaviour
+{
+    [SerializeField] float m_speed = 1f;
+    public PlayerController enemy;
+
+    void Start()
+    {
+        // 速度ベクトルを求める
+       // GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (enemy)
+        {
+            Vector2 v = enemy.transform.position - this.transform.position;
+            v = v.normalized * m_speed;
+
+            // 速度ベクトルをセットする
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.velocity = v;
+        }
+        else
+        {
+            // プレイヤーが居なかったら、すぐ消してしまう
+            Destroy(this.gameObject);
+        }
+    }
+}
